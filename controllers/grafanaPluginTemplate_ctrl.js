99bee5bb56a77022b8d91e89ed44b50c38658ec5
Billy Ernest <bernest@gridprotectionalliance.org>
2017-12-08 08:54:20 -0500

    Added grafana events to template

--- controllers/grafanaPluginTemplate_ctrl.js
+++ controllers/grafanaPluginTemplate_ctrl.js
@@ -1,30 +1,48 @@
 ///<reference path="../node_modules/grafana-sdk-mocks/app/headers/common.d.ts" />
-System.register(['app/plugins/sdk'], function(exports_1) {
-    var __extends = (this && this.__extends) || function (d, b) {
-        for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-    var sdk_1;
-    var GrafanaPluginTemplateCtrl;
-    return {
-        setters:[
-            function (sdk_1_1) {
-                sdk_1 = sdk_1_1;
-            }],
-        execute: function() {
-            //import { varName } from '../js/constants'   // import constants from constant file using this format
-            GrafanaPluginTemplateCtrl = (function (_super) {
-                __extends(GrafanaPluginTemplateCtrl, _super);
-                function GrafanaPluginTemplateCtrl($scope, $injector, $rootScope) {
-                    _super.call(this, $scope, $injector);
-                    this.$rootScope = $rootScope;
-                }
-                GrafanaPluginTemplateCtrl.templateUrl = 'partials/module.html';
-                return GrafanaPluginTemplateCtrl;
-            })(sdk_1.MetricsPanelCtrl);
-            exports_1("GrafanaPluginTemplateCtrl", GrafanaPluginTemplateCtrl);
-        }
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const sdk_1 = require("app/plugins/sdk");
+//import { varName } from '../js/constants'   // import constants from constant file using this format
+class GrafanaPluginTemplateCtrl extends sdk_1.MetricsPanelCtrl {
+    constructor($scope, $injector, $rootScope) {
+        super($scope, $injector);
+        this.$rootScope = $rootScope;
+        this.events.on('init-edit-mode', this.onInitEditMode.bind(this));
+        this.events.on('panel-teardown', this.onPanelTeardown.bind(this));
+        this.events.on('render', this.onRender.bind(this));
+        this.events.on('panel-initialized', this.onPanelInitialized.bind(this));
+        this.events.on('data-received', this.onDataRecieved.bind(this));
+        //this.events.on('data-snapshot-load', console.log('data-snapshot-load'));
+        this.events.on('data-error', this.onDataError.bind(this));
+        this.events.on('refresh', this.onRefresh.bind(this));
     }
-});
+    // #region Events from Graphana Handlers
+    onInitEditMode() {
+        //console.log('init-edit-mode');
+    }
+    onPanelTeardown() {
+        //console.log('panel-teardown');
+    }
+    onPanelInitialized() {
+        //console.log('panel-initialized');
+    }
+    onRefresh() {
+        //console.log('refresh');
+    }
+    onResize() {
+        var ctrl = this;
+        //console.log('refresh');
+    }
+    onRender() {
+        //console.log('render');
+    }
+    onDataRecieved(data) {
+        //console.log('data-recieved');
+    }
+    onDataError(msg) {
+        //console.log('data-error');
+    }
+}
+GrafanaPluginTemplateCtrl.templateUrl = 'partials/module.html';
+exports.GrafanaPluginTemplateCtrl = GrafanaPluginTemplateCtrl;
 //# sourceMappingURL=grafanaPluginTemplate_ctrl.js.map
--- controllers/grafanaPluginTemplate_ctrl.ts
+++ controllers/grafanaPluginTemplate_ctrl.ts
@@ -9,5 +9,51 @@ export class GrafanaPluginTemplateCtrl extends MetricsPanelCtrl{
 
     constructor($scope, $injector, private $rootScope) {
         super($scope, $injector);
+        this.events.on('init-edit-mode', this.onInitEditMode.bind(this));
+        this.events.on('panel-teardown', this.onPanelTeardown.bind(this));
+        this.events.on('render', this.onRender.bind(this));
+        this.events.on('panel-initialized', this.onPanelInitialized.bind(this));
+        this.events.on('data-received', this.onDataRecieved.bind(this));
+        //this.events.on('data-snapshot-load', console.log('data-snapshot-load'));
+        this.events.on('data-error', this.onDataError.bind(this));
+        this.events.on('refresh', this.onRefresh.bind(this));
+
     }
+
+    // #region Events from Graphana Handlers
+    onInitEditMode() {
+        //console.log('init-edit-mode');
+    }
+
+    onPanelTeardown() {
+        //console.log('panel-teardown');
+    }
+
+    onPanelInitialized() {
+        //console.log('panel-initialized');
+    }
+
+    onRefresh() {
+        //console.log('refresh');
+    }
+
+    onResize() {
+        var ctrl = this;
+        //console.log('refresh');
+    }
+
+    onRender() {
+        //console.log('render');
+
+    }
+
+    onDataRecieved(data) {
+        //console.log('data-recieved');
+    }
+
+    onDataError(msg) {
+        //console.log('data-error');
+    }
+    // #endregion
+
 }

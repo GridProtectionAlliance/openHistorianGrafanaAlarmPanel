13717d4e69571253c9d5d2018b0b710ea19bc17d
Billy Ernest <bernest@gridprotectionalliance.org>
2017-12-15 16:02:00 -0500

    initial commit

--- controllers/openHistorianGrafanaAlarmPanel_ctrl.ts
+++ controllers/openHistorianGrafanaAlarmPanel_ctrl.ts
@@ -1,10 +1,34 @@
-﻿///<reference path="../node_modules/grafana-sdk-mocks/app/headers/common.d.ts" />
+﻿//******************************************************************************************************
+//  openHistorianGrafanaAlarmPanel.ts - Gbtc
+//
+//  Copyright © 2017, Grid Protection Alliance.  All Rights Reserved.
+//
+//  Licensed to the Grid Protection Alliance (GPA) under one or more contributor license agreements. See
+//  the NOTICE file distributed with this work for additional information regarding copyright ownership.
+//  The GPA licenses this file to you under the MIT License (MIT), the "License"; you may not use this
+//  file except in compliance with the License. You may obtain a copy of the License at:
+//
+//      http://opensource.org/licenses/MIT
+//
+//  Unless agreed to in writing, the subject software distributed under the License is distributed on an
+//  "AS-IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. Refer to the
+//  License for the specific language governing permissions and limitations.
+//
+//  Code Modification History:
+//  ----------------------------------------------------------------------------------------------------
+//  12/15/2017 - Billy Ernest
+//       Generated original version of source code.
+//
+//******************************************************************************************************
+
+
+///<reference path="../node_modules/grafana-sdk-mocks/app/headers/common.d.ts" />
 
 import { MetricsPanelCtrl } from 'app/plugins/sdk';
 
 //import { varName } from '../js/constants'   // import constants from constant file using this format
 
-export class GrafanaPluginTemplateCtrl extends MetricsPanelCtrl{
+export class OpenHistorianGrafanaAlarmPanel extends MetricsPanelCtrl{
     static templateUrl:string = 'partials/module.html';
 
     constructor($scope, $injector, private $rootScope) {

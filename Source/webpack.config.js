8c74a856fa0c2bb2e3237062dd5bb72396ae0288
Christoph Lackner <clackner@gridprotectionalliance.org>
2019-12-13 11:16:28 -0500

    Fixed Security Vulerability

--- Source/webpack.config.js
+++ Source/webpack.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 const webpack = require('webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const ngAnnotatePlugin = require('ng-annotate-webpack-plugin');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 
